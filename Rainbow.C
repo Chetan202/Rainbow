b48c1d5dfcc2c25ef239103b72a1b584fb12c03e
chetanjha <52883959+Chetan202@users.noreply.github.com>
2021-03-12 19:18:16 +0530

    Create Rainbow.C

---
+++ Rainbow.C
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+#include<graphics.h>
+#include<dos.h>
+void rainbow(){
+    int gdriver = DETECT,gmode;
+    int x,y,i;
+    initgraph(&gdriver,gmode,"C:\\TC\\BGI");
+    x=getmaxx()/2;
+    x=getmaxy()/2;
+    for(i=30;i<200;i++){
+        delay(100);
+        setcolor(i/10);
+        arc(x,y,0,180,i-10);
+    }
+}
+
+int main(){
+    rainbow();
+    return 0;
+}
+
+
